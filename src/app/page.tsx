ba477a5dab5388a8e0bf1dbd0f4de0730a168146
Hugo Cruz <hugocruzlfc@gmail.com>
2025-03-13 17:20:59 +0100

    style: add fav icon and assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>hi</main>;
+  return (
+    <main>
+      hi
+      <Button>Click me</Button>
+    </main>
+  );
 }
