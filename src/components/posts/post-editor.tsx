292d7006f46ea4e5dd380eb4ab51c08b9db86716
Hugo Cruz <hugocruzlfc@gmail.com>
2025-03-14 15:06:56 +0100

    feat: add post editor and insert db post

---
+++ src/components/posts/post-editor.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+import Placeholder from "@tiptap/extension-placeholder";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+import { submitPost } from "@/actions/submit-post-action";
+import { useSession } from "../session-provider";
+import UserAvatar from "../user-avatar";
+import "./styles.css";
+
+export default function PostEditor() {
+  const { user } = useSession();
+
+  const editor = useEditor({
+    immediatelyRender: false,
+    extensions: [
+      StarterKit.configure({
+        bold: false,
+        italic: false,
+      }),
+      Placeholder.configure({
+        placeholder: "What's crack-a-lackin'?",
+      }),
+    ],
+  });
+
+  const input =
+    editor?.getText({
+      blockSeparator: "\n",
+    }) || "";
+
+  async function onSubmit() {
+    await submitPost(input);
+    editor?.commands.clearContent();
+  }
+
+  return (
+    <div className="bg-card flex flex-col gap-5 rounded-2xl p-5 shadow-sm">
+      <div className="flex gap-5">
+        <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
+        <EditorContent
+          editor={editor}
+          className="bg-secondary max-h-[20rem] w-full overflow-y-auto rounded-2xl px-5 py-3"
+        />
+      </div>
+      <div className="flex justify-end">
+        <Button
+          onClick={onSubmit}
+          disabled={!input.trim()}
+          className="min-w-20"
+        >
+          Post
+        </Button>
+      </div>
+    </div>
+  );
+}
