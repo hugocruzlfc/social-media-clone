074789e0f50627d15e98ff337b273e937ecd70ac
Hugo Cruz <hugocruzlfc@gmail.com>
2025-03-20 12:35:46 +0100

    feat: add ky insatnce

--- src/components/posts/for-you-feed.tsx
+++ src/components/posts/for-you-feed.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import kyInstance from "@/lib/ky-instance";
 import { PostData } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
@@ -8,13 +9,7 @@ import Post from "./post";
 export default function ForYouFeed() {
   const query = useQuery<PostData[]>({
     queryKey: ["post-feed", "for-you"],
-    queryFn: async () => {
-      const res = await fetch("/api/posts/for-you");
-      if (!res.ok) {
-        throw Error(`Request failed with status code ${res.status}`);
-      }
-      return res.json();
-    },
+    queryFn: kyInstance.get("/api/posts/for-you").json<PostData[]>,
   });
 
   if (query.status === "pending") {
