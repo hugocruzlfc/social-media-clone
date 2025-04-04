8cee078b1064bc0e2972fbe5a807199487376ce0
Hugo Cruz <hugocruzlfc@gmail.com>
2025-04-04 12:57:28 +0200

    feat: add like butons

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,6 +6,7 @@ export const API_URL = {
   POST_BY_USER: (userId: string) => `/api/users/${userId}/posts` as const,
   USER_BY_USERNAME: (username: string) =>
     `/api/users/username/${username}` as const,
+  LIKES_BY_POST: (postId: string) => `/api/posts/${postId}/likes` as const,
 };
 
 export const POSTS_PER_PAGE = 10;
